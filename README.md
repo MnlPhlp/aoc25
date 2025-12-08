dd0d54a5187709fdf3cd4dabbb4cd18ddf9f2606
MnlPhlp <online@philipp-manuel.de>
2025-12-08 22:55:00 +0100

    day 7 part 1

--- README.md
+++ README.md
@@ -8,19 +8,8 @@ Executed by `cargo run --release` on Framework Laptop 14 with `12th Gen Intel(R)
 ## Results:
 day | result 1        | result 2        | time      | % overall 
 --: | :-------------: | :--------------:| --------: | :--------
-  1 | 1100            | 6358            |  696.20µs | 6.07 %
-  2 | 64215794229     | 85513235135     |    2.39ms | 20.81 %
-  3 | 16812           | 166345822896410 |  329.42µs | 2.87 %
-  4 | 1419            | 8739            |    6.66ms | 58.04 %
-  5 | 737             | 357485433193284 |  475.73µs | 4.14 %
-  6 | 4722948564882   | 9581313737063   |  712.45µs | 6.21 %
-  7 | Not implemented | Not implemented |    6.79µs | 0.05 %
-  8 | Not implemented | Not implemented |    5.90µs | 0.04 %
-  9 | Not implemented | Not implemented |    6.19µs | 0.05 %
- 10 | Not implemented | Not implemented |    5.41µs | 0.04 %
- 11 | Not implemented | Not implemented |    4.92µs | 0.03 %
- 12 | Not implemented | Not implemented |    5.28µs | 0.04 %
+  7 | 21              | 21              |   48.08µs | 45.28 %
 
-Overall Time: 11.467746ms
+Overall Time: 106.312µs
 
-Summed Time: 11.291893ms
+Summed Time: 48.075µs
--- src/days/day07.rs
+++ src/days/day07.rs
@@ -1,19 +1,68 @@
 use crate::types::DaySolver;
 
+pub struct Input<'a> {
+    start_col: usize,
+    width: usize,
+    rest: &'a str,
+}
+
 pub struct Solver;
 
 impl<'a> DaySolver<'a> for Solver {
-    type Input = &'a str;
+    type Input = Input<'a>;
 
     fn parse_input(input: &'a str, _test: bool) -> Self::Input {
-        input
+        let (first_line, rest) = input.split_once('\n').unwrap_or((input, ""));
+        let width = first_line.len();
+        let start_col = first_line.find('S').unwrap_or(width);
+        Input {
+            start_col,
+            width,
+            rest,
+        }
     }
 
-    fn solve1(&self, _input: &Self::Input, _test: bool) -> String {
-        "Not implemented".to_string()
+    fn solve1(&self, input: &Self::Input, _test: bool) -> String {
+        let mut splits = 0;
+        let mut active = vec![false; input.width];
+        active[input.start_col] = true;
+        for line in input.rest.lines() {
+            for (i, &c) in line.as_bytes().iter().enumerate() {
+                if c == b'^' && active[i] {
+                    splits += 1;
+                    active[i] = false;
+                    if i > 0 {
+                        active[i - 1] = true;
+                    }
+                    if i + 1 < input.width {
+                        active[i + 1] = true;
+                    }
+                }
+            }
+        }
+
+        splits.to_string()
     }
 
-    fn solve2(&self, _input: &Self::Input, _test: bool) -> String {
-        "Not implemented".to_string()
+    fn solve2(&self, input: &Self::Input, _test: bool) -> String {
+        let mut paths = 0;
+        let mut active = vec![false; input.width];
+        active[input.start_col] = true;
+        for line in input.rest.lines() {
+            for (i, &c) in line.as_bytes().iter().enumerate() {
+                if c == b'^' && active[i] {
+                    paths += 1;
+                    active[i] = false;
+                    if i > 0 {
+                        active[i - 1] = true;
+                    }
+                    if i + 1 < input.width {
+                        active[i + 1] = true;
+                    }
+                }
+            }
+        }
+
+        paths.to_string()
     }
 }
