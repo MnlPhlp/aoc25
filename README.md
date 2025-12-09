b13c561d3b5b4714e146cf11973daabfb0f030c5
MnlPhlp <online@philipp-manuel.de>
2025-12-09 08:24:59 +0100

    day 7 part 2

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ Executed by `cargo run --release` on Framework Laptop 14 with `12th Gen Intel(R)
 ## Results:
 day | result 1        | result 2        | time      | % overall 
 --: | :-------------: | :--------------:| --------: | :--------
-  7 | 21              | 21              |   48.08µs | 45.28 %
+  7 | 1711            | 36706966158365  |  477.71µs | 93.35 %
 
-Overall Time: 106.312µs
+Overall Time: 511.218µs
 
-Summed Time: 48.075µs
+Summed Time: 477.709µs
--- src/days/day07.rs
+++ src/days/day07.rs
@@ -44,25 +44,38 @@ impl<'a> DaySolver<'a> for Solver {
         splits.to_string()
     }
 
-    fn solve2(&self, input: &Self::Input, _test: bool) -> String {
-        let mut paths = 0;
-        let mut active = vec![false; input.width];
-        active[input.start_col] = true;
+    fn solve2(&self, input: &Self::Input, test: bool) -> String {
+        let mut active = vec![0u64; input.width];
+        active[input.start_col] = 1;
         for line in input.rest.lines() {
             for (i, &c) in line.as_bytes().iter().enumerate() {
-                if c == b'^' && active[i] {
-                    paths += 1;
-                    active[i] = false;
+                if c == b'^' && active[i] > 0 {
                     if i > 0 {
-                        active[i - 1] = true;
+                        active[i - 1] += active[i];
                     }
                     if i + 1 < input.width {
-                        active[i + 1] = true;
+                        active[i + 1] += active[i];
+                    }
+                    active[i] = 0;
+                }
+            }
+            if test {
+                for (i, &c) in line.as_bytes().iter().enumerate() {
+                    if i > 0 {
+                        print!(" ");
+                    }
+                    if active[i] > 0 {
+                        print!("{}", active[i]);
+                    } else if c == b'^' {
+                        print!("^");
+                    } else {
+                        print!(".");
                     }
                 }
+                println!();
             }
         }
 
-        paths.to_string()
+        active.iter().sum::<u64>().to_string()
     }
 }
